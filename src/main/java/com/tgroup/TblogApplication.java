8f1667bacc50ed0edc61df36eb0833230c04abda
Terry <lujunjie400428@126.com>
2017-03-12 00:26:27 +0800

    n/a

---
+++ src/main/java/com/tgroup/TblogApplication.java
@@ -0,0 +1,12 @@
+package com.tgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TblogApplication.class, args);
+	}
+}
---
+++ src/main/java/com/tgroup/controller/TblogController.java
@@ -0,0 +1,22 @@
+package com.tgroup.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Terry on 17-3-11.
+ */
+@Controller
+@EnableAutoConfiguration
+@RequestMapping("/web/article")
+public class TblogController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String getArticles() {
+        return "Hello World!";
+    }
+
+}
