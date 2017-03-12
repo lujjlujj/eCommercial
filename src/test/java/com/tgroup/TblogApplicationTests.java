c8ceb86af7b5897c9c18bdbedce4c8b43efe947e
Terry <lujunjie400428@126.com>
2017-03-12 12:34:27 +0800

    Add new test case to trigger Travis

--- src/test/java/com/tgroup/TblogApplicationTests.java
+++ src/test/java/com/tgroup/TblogApplicationTests.java
@@ -1,9 +1,13 @@
 package com.tgroup;
 
+import junit.framework.Assert;
+import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.constraints.AssertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +17,8 @@ public class TblogApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void alwaysTrue() {
+		TestCase.assertTrue(true);
+	}
 }
