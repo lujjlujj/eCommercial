a078d8966d45aa988724341e3cc52d7b79213c5a
Terry <lujunjie400428@126.com>
2017-04-23 23:09:55 +0800

    Refactor

--- src/main/java/com/ebuy/TblogApplication.java
+++ src/main/java/com/ebuy/TblogApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TblogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TblogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TblogApplication.class, args);
+    }
+
+
 }
--- src/main/java/com/ebuy/security/WebSecurityConfig.java
+++ src/main/java/com/ebuy/security/WebSecurityConfig.java
@@ -2,29 +2,47 @@ package com.ebuy.security;
 
 import com.fasterxml.jackson.annotation.JacksonInject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.CompositeFilter;
+import javax.servlet.Filter;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by terry on 2017/4/23.
  */
 @Configuration
 @EnableWebSecurity
+@RestController
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @RequestMapping("/user")
     public Principal user(Principal user) {
         return user;
     }
+
+    @Autowired
+    OAuth2ClientContext oauth2ClientContext;
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
@@ -34,7 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers()
                     .and()
                 .authorizeRequests()
-                    .antMatchers("/login.html", "/login", "/js/**", "/css/**")
+                    .antMatchers("/login", "/login/**", "/js/**", "/css/**")
                     .permitAll().anyRequest().authenticated()
                     .and()
                 .formLogin()
@@ -47,11 +65,50 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
                     .and()
                 .csrf()
-                    .csrfTokenRepository(securityCsrfTokenRepository);//and().csrf().disable();
+                    .csrfTokenRepository(securityCsrfTokenRepository)
+                    .and()
+                .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);//and().csrf().disable();
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    }
+
+    private Filter ssoFilter() {
+
+        CompositeFilter filter = new CompositeFilter();
+        List<Filter> filters = new ArrayList<>();
+
+//        OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
+//        OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
+//        facebookFilter.setRestTemplate(facebookTemplate);
+//        UserInfoTokenServices tokenServices = new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId());
+//        tokenServices.setRestTemplate(facebookTemplate);
+//        facebookFilter.setTokenServices(tokenServices);
+//        filters.add(facebookFilter);
+
+        OAuth2ClientAuthenticationProcessingFilter githubFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/github");
+        OAuth2RestTemplate githubTemplate = new OAuth2RestTemplate(github(), oauth2ClientContext);
+        githubFilter.setRestTemplate(githubTemplate);
+        UserInfoTokenServices tokenServices = new UserInfoTokenServices(githubResource().getUserInfoUri(), github().getClientId());
+        tokenServices.setRestTemplate(githubTemplate);
+        githubFilter.setTokenServices(tokenServices);
+        filters.add(githubFilter);
+        filter.setFilters(filters);
+        return filter;
+
+    }
+
+    @Bean
+    @ConfigurationProperties("github.client")
+    public AuthorizationCodeResourceDetails github() {
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    @Bean
+    @ConfigurationProperties("github.resource")
+    public ResourceServerProperties githubResource() {
+        return new ResourceServerProperties();
     }
 }
